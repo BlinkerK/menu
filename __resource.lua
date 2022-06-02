fcba3d86b5fa200e3d991b9e681c5d38db944f2d
BlinkerK <105089475+BlinkerK@users.noreply.github.com>
2022-06-02 14:26:46 -0300

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,9 @@
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+description 'Mode Menu Phanta v.1'
+client_scripts {
+	'gui.lua',
+	'client.lua',
+	'config.lua',
+}
+
+server_script 'server.lua'
